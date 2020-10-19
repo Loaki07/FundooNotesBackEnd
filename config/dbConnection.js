c777917a34dfa696f0450475d9d3504bad128dab
sendo07 <loakesh.i@gmail.com>
2020-10-19 09:42:27 +0530

    refactor: Using Event based Db connection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -9,12 +9,21 @@ dotenv.config();
 const connectDB = async () => {
   try {
     mongoose.set('useCreateIndex', true);
+
+    mongoose.connection.on('connected', () => {
+      logger.info('Connected to MongoDB!');
+      console.log('Connected to MongoDB!');
+    });
+
+    mongoose.connection.on('disconnected', () => {
+      logger.info('MongoDB Connection disconnected!');
+      console.log('MongoDB Connection disconnected!');
+    });
+
     await mongoose.connect(process.env.MONGODB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    logger.info('Connected to MongoDB!');
-    console.log('Connected to MongoDB!');
   } catch (error) {
     logger.error('Could not Connect to MongoDB...', error);
     console.log('Could not Connect to MongoDB...', error);
